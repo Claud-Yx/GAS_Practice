7ce0936e631a370bc3fa6063f32ceea25cb34b8a
Cloudyx <durjs96@gmail.com>
2024-03-08 22:39:35 +0900

    Create Project

---
+++ Source/GAS_Practice/GAS_PracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAS_PracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_PRACTICE_API AGAS_PracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
